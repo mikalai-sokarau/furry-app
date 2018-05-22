54456081c95e6872912d304aca32d7d7348a4f21
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2018-05-22 15:36:54 +0300

    remove redundant variable declaration in searchBar

--- app/directives/searchBar/searchBar.controller.js
+++ app/directives/searchBar/searchBar.controller.js
@@ -28,7 +28,6 @@ export default function($state, $stateParams, $transitions) {
     };
 
     $transitions.onSuccess({}, () => {
-        this.isFirstPage = window.history.length <= 2;
         this.searchText = $stateParams.text || $stateParams.name;
     });
 }
